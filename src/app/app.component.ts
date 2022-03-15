301084892d6a14611be0bc532a2a88030bc3b46d
Ronith Sinha <ronithsinha12@gmail.com>
2022-03-15 12:25:39 +0530

    updated new relic-debugging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,37 @@ declare var NewRelic;
 export class AppComponent {
   constructor() {
     console.log('before new relic init');
+
     try {
       NewRelic.initialize();
     } catch (error) {
       console.log(error);
     }
+
+    try {
+      NewRelic.enableCrashReporting(true);
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
+      NewRelic.setApplicationVersion('2.3.32');
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
+      NewRelic.recordMetricWithName('metric1', 'category1');
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
+      NewRelic.crashNow();
+    } catch (error) {
+      console.log(error);
+    }
+
     console.log('after new relic init');
   }
 }
